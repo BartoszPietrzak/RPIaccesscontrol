5e76fbce3bef6417e6854d86938289712d461cca
Bartosz Pietrzak <bartosz.pietrzak@bwigroup.com>
2018-07-30 13:07:54 +0200

    read from file

--- RPIac.py
+++ RPIac.py
@@ -1,1 +1,14 @@
 #Program do odczytu listy z pliku
+listfile = open('list.txt','r')
+try:
+     list = listfile.read()
+finally:
+    listfile.close()
+
+list = list.split('\n')
+
+for indx in range(0,len(list)):
+    buffer=list[indx].split()
+    list[indx]=int(buffer[3])
+
+#INPUT
